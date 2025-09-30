2e08bb3437a1df3b2ed209a8afd25e2381cec678
Vishal-74 <vishalgupta7494@gmail.com>
2025-09-30 19:36:11 +0530

    added livekit react frontend

--- voxie-frontend/app/(app)/layout.tsx
+++ voxie-frontend/app/(app)/layout.tsx
@@ -15,7 +15,7 @@ export default async function AppLayout({ children }: AppLayoutProps) {
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href="https://livekit.io"
+          href="https://agents.voxhive.ai/"
           className="scale-100 transition-transform duration-300 hover:scale-110"
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -32,10 +32,10 @@ export default async function AppLayout({ children }: AppLayoutProps) {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://docs.livekit.io/agents"
+            href="https://agents.voxhive.ai/"
             className="underline underline-offset-4"
           >
-            LiveKit Agents
+            Orichi Labs
           </a>
         </span>
       </header>
